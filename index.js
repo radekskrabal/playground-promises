6987c40f03b1669d567ae097f59ef65d33e9f3fc
Radek Skrabal <radek@skrabal.me>
2016-02-03 16:13:55 +0100

    Measure refactoring. Add another running func.

--- index.js
+++ index.js
@@ -1,14 +1,71 @@
 "use strict";
 
+class Timer {
+    // private id = null;
+    // private duration = null;
+
+    constructor() {
+        this.id = new Date().getTime().toString();
+    }
+
+    // public
+    start() {
+        performance.mark(this.getStartMarkName());
+    }
+
+    // public
+    end() {
+        performance.mark(this.getEndMarkName());
+        this.calculateDuration();
+
+        performance.clearMarks(this.getStartMarkName());
+        performance.clearMarks(this.getEndMarkName());
+        performance.clearMeasures(this.getMeasureName());
+    }
+
+    // public
+    getDuration(trunc) {
+        if (trunc !== false) {
+            return Math.trunc(this.duration);
+        }
+
+        return this.duration;
+    }
+
+    // private
+    getStartMarkName() {
+        return `start-${this.id}`;
+    }
+
+    // private
+    getEndMarkName() {
+        return `end-${this.id}`;
+    }
+
+    // private
+    getMeasureName() {
+        return `duration-${this.id}`;
+    }
+
+    // private
+    calculateDuration() {
+        performance.measure(this.getMeasureName(), this.getStartMarkName(), this.getEndMarkName());
+
+        let measure = performance.getEntriesByType('measure')[0];
+        this.duration = measure.duration;
+    }
+}
+
 class Person {
     constructor(name) {
         this.name = name;
     }
 
     doSomething() {
-        let iterations = 1000000; // 1M
+        let iterations = 100000,
+            timer = new Timer(); // 100k
 
-        performance.mark('start');
+        timer.start();
         for (let i = 0; i < iterations; i++) {
             let content,
                 text = new Date().getTime().toString();
@@ -22,18 +79,30 @@ class Person {
 
             $('#text').html(content);
         }
-        performance.mark('end');
-        performance.measure('duration', 'start', 'end');
-        let measure = performance.getEntriesByType('measure')[0],
-            duration = measure.duration;
-        console.info(`Do something took ${duration} ms`);
+        timer.end();
+        console.info(`doSomething took ${timer.getDuration()} ms`);
+    }
 
-        performance.clearMarks();
-        performance.clearMeasures();
+    doSomethingElse() {
+        let iterations = 1000000,  // 1M
+            timer = new Timer();
+
+        timer.start();
+        for (let i = 0; i < iterations; i++) {
+            let el = $('#text')[0];
+            if (i % 2 === 0) {
+                el.classList.add('somecls');
+            } else {
+                el.classList.remove('somecls');
+            }
+        }
+        timer.end();
+        console.info(`doSomethingElse took ${timer.getDuration()} ms`);
     }
 }
 
 $(document).ready(function() {
     let worker = new Person('Long Running Worker');
     worker.doSomething();
+    worker.doSomethingElse();
 });
